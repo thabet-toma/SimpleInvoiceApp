9dc971c7a9dff98e4895383027e23464d6f7c009
thabet-toma <thapet64@gmail.com>
2026-06-16 21:49:23 +0300

    Fix React TS error in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Login';
 import Dashboard from './Dashboard';
